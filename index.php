22506d1da127195d1849392b5a93210e237fb70e
Asionsolver <asionsolver274@gmail.com>
2022-02-17 18:07:39 +0600

    Modified index.html

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php 
+    /*
+    $food = 'Pizza';
+    $Food = 'Berger';
+    echo "I Love First Food " . $food; 
+    echo "I Love First Food " . $Food; 
+    */
+
+    $notation = 'snack_case';
+    $notation2 = 'camelCase';
+    $notation3 = 'PascalCase';
+    echo "ashisKumar " . $notation2;
+    echo "ashis_kumar " . $notation; 
+    echo "AshisKumar " . $notation3; 
+    
+    ?>
+
+    <!-- <?php echo 'Hello PHP'; ?>
+    <?php ECHO "Hello PHP"; ?>
+    <?php Echo "Hello PHP"; ?> -->
+</body>
+
+</html>
