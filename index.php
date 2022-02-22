725fc2f01a46e7d217782167ff717dab12b24428
Asionsolver <asionsolver274@gmail.com>
2022-02-22 08:46:40 +0600

    Modifie index.php file

--- index.php
+++ index.php
@@ -17,63 +17,101 @@
     echo "I Love First Food " . $Food; 
     */
 
-    $notation = 'snack_case';
-    $_notation2 = 'camelCase';
-    $notation3 = 'PascalCase';
+    // $notation = 'snack_case';
+    // $_notation2 = 'camelCase';
+    // $notation3 = 'PascalCase';
 
 
-    echo "1. ashisKumar ---- " . $_notation2 .'<br>';
-    echo "2. ashis_kumar ---- " . $notation. '<br>'; 
-    echo "3. AshisKumar ---- " . $notation3. '<br>'; 
+    // echo "1. ashisKumar ---- " . $_notation2 .'<br>';
+    // echo "2. ashis_kumar ---- " . $notation. '<br>'; 
+    // echo "3. AshisKumar ---- " . $notation3. '<br>'; 
 
-    $age = 24;
-    echo"I am a undergraduate student.My age is ".$age. "<br>";
+    // $age = 24;
+    // echo"I am a undergraduate student.My age is ".$age. "<br>";
 
     // Two variable Sum
 
-    $x = 16;
-    $y = 24;
+    // $x = 16;
+    // $y = 24;
 
-    echo $x + $y. "<br>";
-    print $x + $y. "<br>";
-    $fastFood = 'pizza';
-    echo $fastFood.'<br>';
-    $details = print ('I love Pizza'.'<br>');
-    echo $details.'<br>';
-    $name = 'Ashis';
-    $number1 = 30;
-    $floatNumber = 30.5;
-    $bool = true;
-    $arr1 = array('apple', 'banana','jackfruit');
+    // echo $x + $y. "<br>";
+    // print $x + $y. "<br>";
+    // $fastFood = 'pizza';
+    // echo $fastFood.'<br>';
+    // $details = print ('I love Pizza'.'<br>');
+    // echo $details.'<br>';
+    // $name = 'Ashis';
+    // $number1 = 30;
+    // $floatNumber = 30.5;
+    // $bool = true;
+    // $arr1 = array('apple', 'banana','jackfruit');
 
-    var_dump($name);
-    var_dump($number1);
+    // var_dump($name);
+    // var_dump($number1);
     // echo'<br>';
-    var_dump($floatNumber);
+    // var_dump($floatNumber);
     // echo'<br>';
-    var_dump($bool);
+    // var_dump($bool);
+    
+    // var_dump($arr1);
+    
+    // class phone {
+    //     var $model;
+    //     function phoneModel($number){
+    //         global $model;
+    //         $model = $number;
+    //         echo 'This is '.$model. '<br>';
+    //     }
+    // }
+    
+    // $apple = new phone;
+    // $apple->phoneModel('iPhone 14');
+    
+    // $realMe = new phone;
+    // $realMe->phoneModel('REALME 3 PRO');
+    
+    // $samsung = new phone;
+    // $samsung->phoneModel('Samsung S22 Ultra Pro Max');
+    
+    // var_dump($apple);
+    
+    // echo strlen("I am a undergraduate student.");
     // echo'<br>';
-    var_dump($arr1);
+    // echo str_word_count("I am a undergraduate student.");
+    // echo'<br>';
+    // echo strrev("I am a undergraduate student.");
+    // echo'<br>';
+    // echo strpos("I am a undergraduate student." , "student");
+    // echo'<br>';
+    // echo str_replace("undergraduate","graduate","I am a undergraduate student.");
+    // echo'<br>';
+    // $test = strpos("I am a undergraduate student." , "a");
+    // var_dump($test)
     
-    class phone {
-        var $model;
-        function phoneModel($number){
-            global $model;
-            $model = $number;
-            echo 'This is '.$model. '<br>';
-        }
-    }
-    
-    $apple = new phone;
-    $apple->phoneModel('iPhone 14');
-    
-    $realMe = new phone;
-    $realMe->phoneModel('REALME 3 PRO');
-    
-    $samsung = new phone;
-    $samsung->phoneModel('Samsung S22 Ultra Pro Max');
-    
-    var_dump($apple);
+    // echo (pi());
+    // echo'<br>';
+    // echo (min(15,3,65,9,6,35,8,5,9,6,66,699,789));
+    // echo'<br>';
+    // echo (max(15,3,65,9,89,6,35,8,5,9,6,66,699,789));
+    // echo'<br>';
+    // echo (abs(-10));
+    // echo'<br>';
+    // echo (sqrt(16));
+    // echo'<br>';
+    // echo (round(pi()));
+    // echo'<br>';
+    // echo (rand(10,50));
+
+    define('a',13);
+    echo(a);
+    var_dump(a);
+    define('b','13');
+    echo(b);
+    var_dump(b);
+
+    define('car',['RollRoyce','RangeRover','BMW','BENZ']);
+    var_dump(car);
+    echo('My dream car is '.car[1]);
 
     
     ?>
